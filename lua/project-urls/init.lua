2708a275f8a1e9075b07a5fe1adc2feafed0ee6f
Edgar Latorre <me@edgarlatorre.com.br>
2024-02-02 17:15:45 +0100

    Implement close menu and open url
    Implement function to open a url from the buffer and a function to close the window and buffer.

--- lua/project-urls/init.lua
+++ lua/project-urls/init.lua
@@ -1,4 +1,4 @@
-local context_manager = require("plenary.context_manager")
+local Path = require("plenary.path")
 
 local Menu = {}
 
@@ -15,6 +15,12 @@ function Menu:get_content(path)
     path = vim.loop.cwd() .. "/.project-urls"
   end
 
+  local file_path = Path:new(path)
+
+  if not file_path:exists() then
+    return {}
+  end
+
   local urls = {}
   for line in io.lines(path) do
     table.insert(urls, line)
@@ -23,18 +29,33 @@ function Menu:get_content(path)
   return urls
 end
 
-function Menu:toggle()
-  Menu:open_menu()
-end
-
 function Menu:open_menu()
   if self.content == nil then
     self.content = Menu:get_content()
   end
 
-  self.buffer = vim.api.nvim_create_buf(false, true)
+  if self.buffer == nil then
+    self.buffer = vim.api.nvim_create_buf(false, true)
+  end
+
+  vim.api.nvim_buf_set_keymap(
+    self.buffer,
+    "n",
+    "<ESC>",
+    "<Cmd>lua require('project-urls'):close()<CR>",
+    { silent = true }
+  )
+  vim.api.nvim_buf_set_keymap(
+    self.buffer,
+    "n",
+    "<CR>",
+    "<Cmd>lua require('project-urls'):open_url()<CR>",
+    { silent = true }
+  )
+
   vim.api.nvim_buf_set_lines(self.buffer, 0, -1, true, self.content)
   vim.api.nvim_buf_set_option(self.buffer, "readonly", true)
+
   local height = 8
   local width = 69
 
@@ -46,6 +67,31 @@ function Menu:open_menu()
     height = height,
     border = "single",
   })
+end
+
+function Menu:close()
+  if self.buffer ~= nil and vim.api.nvim_buf_is_valid(self.buffer) then
+    vim.api.nvim_buf_delete(self.buffer, { force = true })
+  end
+
+  if self.window_id ~= nil and vim.api.nvim_win_is_valid(self.window_id) then
+    vim.api.nvim_win_close(self.window_id, true)
+  end
+
+  self.window_id = nil
+  self.buffer = nil
+end
+
+function Menu:open_url()
+  local index = vim.fn.line(".")
+
+  for content_idx, url in ipairs(self.content) do
+    if index == content_idx then
+      vim.cmd(":silent! !open " .. url)
+      self:close()
+      break
+    end
+  end
 end
 
 return Menu
